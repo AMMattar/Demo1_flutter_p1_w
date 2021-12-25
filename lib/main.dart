d0cb8228424e97dbaa1f19abe30b39e51259e055
crypto <ahmed.m4busines@gmail.com>
2021-12-24 17:50:11 -0800

    add timer

--- lib/main.dart
+++ lib/main.dart
@@ -4,63 +4,95 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var textIndex = 0;
+  void testFunc() {
+    Future.delayed(const Duration(milliseconds: 3500), () {
+// Here you can write your code
+
+      setState(() {
+        // Here you can write your code for open new view
+        textIndex = 1;
+      });
+    });
+  }
+
+  @override
+  var test = ListView(
+    children: [
+      Center(
+        child: Row(children: [
+          Container(
+            margin: EdgeInsets.all(8.0),
+            child: Image(image: AssetImage("assets/1.png")),
+          ),
+        ]),
+      ),
+      Center(
+        child: Container(
+            alignment: Alignment(1, 1),
+            child: Text(
+              "مفضلتي",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            )),
+      ),
+      Center(
+        child: Row(children: [
+          Container(
+            //margin: EdgeInsets.all(8.0),
+            child: Image(image: AssetImage("assets/2.png")),
+          ),
+        ]),
+      ),
+      Center(
+        child: Container(
+            alignment: Alignment(1, 1),
+            child: Text(
+              "البحث عن حالة",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            )),
+      ),
+      Center(
+        child: Row(children: [
+          Container(
+            //margin: EdgeInsets.all(8.0),
+            child: Image(image: AssetImage("assets/3.png")),
+          ),
+        ]),
+      ),
+    ],
+  );
+
+  var test1 = Image(
+    image: AssetImage("assets/1.png"),
+    fit: BoxFit.cover,
+    height: double.infinity,
+    width: double.infinity,
+    alignment: Alignment.center,
+  );
+
   Widget build(BuildContext context) {
+    var newTest2 = [test1, test];
+    testFunc();
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text('Demo APP'),
             ),
-            body: ListView(
-              children: [
-                Center(
-                  child: Row(children: [
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      child: Image(image: AssetImage("assets/1.png")),
-                    ),
-                  ]),
-                ),
-                Center(
-                  child: Container(
-                      alignment: Alignment(1, 1),
-                      child: Text(
-                        "مفضلتي",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      )),
-                ),
-                Center(
-                  child: Row(children: [
-                    Container(
-                      //margin: EdgeInsets.all(8.0),
-                      child: Image(image: AssetImage("assets/2.png")),
-                    ),
-                  ]),
-                ),
-                Center(
-                  child: Container(
-                      alignment: Alignment(1, 1),
-                      child: Text(
-                        "البحث عن حالة",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      )),
-                ),
-                Center(
-                  child: Row(children: [
-                    Container(
-                      //margin: EdgeInsets.all(8.0),
-                      child: Image(image: AssetImage("assets/3.png")),
-                    ),
-                  ]),
-                ),
-              ],
-            )));
+            body: newTest2[textIndex]));
   }
 }
